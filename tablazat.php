ced4ae9f12e5c5b383064c0b182b4c3bf4f5a10f
hegeda <35102729+hegeda@users.noreply.github.com>
2019-02-18 17:24:11 +0100

    Create tablazat.php

---
+++ tablazat.php
@@ -0,0 +1,13 @@
+<?php
+   for ($i = 1; $i <= 10; $i++) {
+	   echo "<tr>";
+	      echo "<td>2019.01.".$i."</td>";
+		  echo "<td>".$i."</td>";
+          echo "<td class='gyum'>wafaef</td>";
+          echo "<td class='zold'>aerfaerf</td>";
+          echo "<td class='gabona'></td>";
+          echo "<td class='hus'>aerfaerf</td>";
+		  echo "<td class='tej'>aerfaerf</td>";
+       echo "</tr>";
+   }
+?>
